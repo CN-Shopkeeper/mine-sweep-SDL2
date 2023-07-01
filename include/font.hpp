3a871554c83de9ebcee25cb27a0164046d60723d
shopkeeper <734212732@qq.com>
2023-07-01 15:59:52 +0800

    add Font

---
+++ include/font.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "pch.hpp"
+
+inline auto FontDestroy = [](TTF_Font* font) { TTF_CloseFont(font); };
+
+class Font final {
+   private:
+    std::unique_ptr<TTF_Font, decltype(FontDestroy)> font_;
+
+   public:
+    Font(const std::string& fontPath, int fontSize);
+};
